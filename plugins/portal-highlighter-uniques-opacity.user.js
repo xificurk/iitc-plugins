ebb528e37cef69fa5a0e20d0744fa721dc1dd2b9
Petr Morávek [Xificurk] <petr@pada.cz>
2016-05-11 17:23:27 +0200

    portal-highlighter-uniques-opacity plugin

---
+++ plugins/portal-highlighter-uniques-opacity.user.js
@@ -0,0 +1,72 @@
+// ==UserScript==
+// @id             iitc-plugin-portal-highlighter-uniques-opacity@xificurk
+// @name           IITC plugin: Highlight unique visits/captures using opacity
+// @category       Highlighter
+// @version        0.1.0.@@DATETIMEVERSION@@
+// @namespace      https://github.com/xificurk/iitc-plugins
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Use stroke and fill opacity to denote player's unique visits and captures. Requires uniques plugin.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
+
+@@PLUGINSTART@@
+//PLUGIN START ////////////////////////////////////////////////////////
+
+
+//use own namespace for plugin
+window.plugin.portalHighlighterUniquesOpacity = function () {};
+
+
+window.plugin.portalHighlighterUniquesOpacity.highlighter = {
+  highlight: function(data) {
+    var guid = data.portal.options.ent[0];
+    var uniqueInfo = window.plugin.uniques.uniques[guid];
+
+    var style = {};
+
+    if(uniqueInfo) {
+      if(uniqueInfo.captured) {
+        // captured (and, implied, visited too) - hide
+        style.fillOpacity = 0;
+        style.opacity = 0.25;
+
+      } else if(uniqueInfo.visited) {
+        style.fillOpacity = 0.2;
+        style.opacity = 1;
+      }
+    } else {
+      // no visit data at all
+      style.fillOpacity = 0.8;
+      style.opacity = 1;
+    }
+
+    data.portal.setStyle(style);
+  },
+
+  setSelected: function(active) {
+    window.plugin.uniques.isHighlightActive = active;
+  }
+}
+
+
+var setup = function() {
+  if(window.plugin.uniques === undefined) {
+    alert("'Portal Highlighter Uniques Opacity' requires 'uniques'");
+    return;
+  }
+
+  window.addPortalHighlighter('Uniques (opacity)', window.plugin.portalHighlighterUniquesOpacity.highlighter);
+}
+
+//PLUGIN END //////////////////////////////////////////////////////////
+
+@@PLUGINEND@@
