0d191f5dc6dd090738b864727bc4ae46b3aa33ff
Petr Morávek <petr@pada.cz>
2018-10-09 22:53:39 +0200

    GDPR uniques plugin

---
+++ dist/uniques-gdpr.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-uniques-gdpr@xificurk
+// @name           IITC plugin: show uniques visit/captures based on GDPR game_log
+// @category       Misc
+// @version        0.1.0.20181009.205252
+// @namespace      https://github.com/xificurk/iitc-plugins
+// @updateURL      https://raw.githubusercontent.com/xificurk/iitc-plugins/master/dist/uniques-gdpr.meta.js
+// @downloadURL    https://raw.githubusercontent.com/xificurk/iitc-plugins/master/dist/uniques-gdpr.user.js
+// @description    [xificurk-2018-10-09-205252] Show uniques visit/captures based on GDPR game_log
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
