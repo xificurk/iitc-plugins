a6279fc4376ca22403761324f297016f956457d1
Petr Morávek <petr@pada.cz>
2023-03-04 16:02:43 +0100

    portal-highlighter-uniques-opacity: refactor to reduce code duplication

--- plugins/portal-highlighter-uniques-opacity.user.js
+++ plugins/portal-highlighter-uniques-opacity.user.js
@@ -30,72 +30,77 @@
 window.plugin.portalHighlighterUniquesOpacity = function () {};
 
 
+window.plugin.portalHighlighterUniquesOpacity.highlight = function(data, styles) {
+  var portalData = data.portal.options.ent[2]
+  var uniqueInfo = null;
+
+  if (portalData[18]) {
+    uniqueInfo = {
+      captured: ((portalData[18] & 0b10) !== 0),
+      visited: ((portalData[18] & 0b11) !== 0)
+    };
+  }
+
+  var style = {};
+
+  if(uniqueInfo) {
+    if(uniqueInfo.captured) {
+      // captured (and, implied, visited too) - hide
+      style = styles.captured;
+
+    } else if(uniqueInfo.visited) {
+      style = styles.visited;
+    }
+  } else {
+    // no visit data at all
+    style = styles.unvisited;
+  }
+
+  data.portal.setStyle(style);
+}
+
+
 window.plugin.portalHighlighterUniquesOpacity.highlighter = {
   highlight: function(data) {
-    var portalData = data.portal.options.ent[2]
-    var uniqueInfo = null;
-
-    if (portalData[18]) {
-      uniqueInfo = {
-        captured: ((portalData[18] & 0b10) !== 0),
-        visited: ((portalData[18] & 0b11) !== 0)
-      };
-    }
-
-    var style = {};
-
-    if(uniqueInfo) {
-      if(uniqueInfo.captured) {
-        // captured (and, implied, visited too) - hide
-        style.fillOpacity = 0;
-        style.opacity = 0.25;
-
-      } else if(uniqueInfo.visited) {
-        style.fillOpacity = 0.2;
-        style.opacity = 1;
+    window.plugin.portalHighlighterUniquesOpacity.highlight(
+      data,
+      {
+        captured: {
+          fillOpacity: 0,
+          opacity: 0.25,
+        },
+        visited: {
+          fillOpacity: 0.2,
+          opacity: 1,
+        },
+        unvisited: {
+          fillOpacity: 0.8,
+          opacity: 1,
+        },
       }
-    } else {
-      // no visit data at all
-      style.fillOpacity = 0.8;
-      style.opacity = 1;
-    }
-
-    data.portal.setStyle(style);
+    );
   }
 }
 
-// write the same function, but inverting the opacities
 window.plugin.portalHighlighterUniquesOpacity.highlighterInverted = {
     highlight: function(data) {
-        var portalData = data.portal.options.ent[2]
-        var uniqueInfo = null;
-
-        if (portalData[18]) {
-            uniqueInfo = {
-            captured: ((portalData[18] & 0b10) !== 0),
-            visited: ((portalData[18] & 0b11) !== 0)
-            };
+      window.plugin.portalHighlighterUniquesOpacity.highlight(
+        data,
+        {
+          captured: {
+            fillOpacity: 0.8,
+            opacity: 1,
+          },
+          visited: {
+            fillOpacity: 0.2,
+            opacity: 1,
+          },
+          unvisited: {
+            fillOpacity: 0,
+            opacity: 0.25,
+          },
         }
-
-        var style = {};
-
-        if(uniqueInfo) {
-            if(uniqueInfo.captured) {
-            // captured (and, implied, visited too) - hide
-            style.fillOpacity = 0.8;
-            style.opacity = 1;
-
-            } else if(uniqueInfo.visited) {
-            style.fillOpacity = 0.2;
-            style.opacity = 1;
-            }
-        } else {
-            // no visit data at all
-            style.fillOpacity = 0;
-            style.opacity = 0.25;
-        }
-
-        data.portal.setStyle(style);
+      );
     }
 }
 
